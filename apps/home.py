15a0e6fd4a2035fb08c0882e1cd004def54b146e
NicolasBelissent <nicolas.belissent@gmail.com>
2023-04-17 15:33:25 +0100

    testing folium map

--- apps/home.py
+++ apps/home.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
+import folium
 
 
 def app():
@@ -14,6 +15,17 @@ def app():
     """
     )
 
-    m = leafmap.Map(locate_control=True)
-    m.add_basemap("ROADMAP")
+    import folium
+
+    political_countries_url = (
+        "http://geojson.xyz/naturalearth-3.3.0/ne_50m_admin_0_countries.geojson"
+    )
+
+    m = folium.Map(location=(30, 10), zoom_start=3, tiles="cartodb positron")
+    folium.GeoJson(political_countries_url).add_to(m)
+
+    m.save("footprint.html")
+
+    # m = leafmap.Map(locate_control=True)
+    # m.add_basemap("ROADMAP")
     m.to_streamlit(height=700)
