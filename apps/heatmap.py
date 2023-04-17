cc1f95182e5bf02b054dc99a0276c084e821efcf
NicolasBelissent <nicolas.belissent@gmail.com>
2023-04-17 16:00:38 +0100

    testing base folium

--- apps/heatmap.py
+++ apps/heatmap.py
@@ -1,19 +1,58 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
+import folium
+import geopandas as gpd
+from streamlit_folium import folium_static
+import numpy as np
 
 
 def app():
 
-    st.title("Heatmap")
+    st.title("Visualiser")
 
-    filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
-    m = leafmap.Map(tiles="stamentoner")
-    m.add_heatmap(
-        filepath,
-        latitude="latitude",
-        longitude="longitude",
-        value="pop_max",
-        name="Heat map",
-        radius=20,
+    st.markdown(
+        """
+    This page can be used to visualise different geospatial indicators.
+
+    """
     )
-    m.to_streamlit(height=700)
+
+    # filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
+    # m = leafmap.Map(tiles="stamentoner")
+    # m.add_heatmap(
+    #     filepath,
+    #     latitude="latitude",
+    #     longitude="longitude",
+    #     value="pop_max",
+    #     name="Heat map",
+    #     radius=20,
+    # )
+
+    
+    # m.to_streamlit(height=700)
+
+
+        
+    # # Load shapefile using geopandas
+    # gdf = gpd.read_file("cheese.shp")
+
+    # # Create a random value for each country
+    # values = np.random.rand(len(gdf))
+
+    # Create a Folium map object
+    m = folium.Map(location=[0, 0], zoom_start=2)
+
+    # # Add a choropleth layer to the map
+    # folium.Choropleth(
+    #     geo_data=gdf,
+    #     data=gdf,
+    #     columns=["iso_a3", values],
+    #     key_on="feature.properties.iso_a3",
+    #     fill_color="YlOrRd",
+    #     fill_opacity=0.7,
+    #     line_opacity=0.2,
+    #     legend_name="Random Values"
+    # ).add_to(m)
+
+    # Display the map in Streamlit using the folium_static method
+    folium_static(m)
--- apps/home.py
+++ apps/home.py
@@ -15,17 +15,17 @@ def app():
     """
     )
 
-    import folium
+    # import folium
 
-    political_countries_url = (
-        "http://geojson.xyz/naturalearth-3.3.0/ne_50m_admin_0_countries.geojson"
-    )
+    # political_countries_url = (
+    #     "http://geojson.xyz/naturalearth-3.3.0/ne_50m_admin_0_countries.geojson"
+    # )
 
-    m = folium.Map(location=(30, 10), zoom_start=3, tiles="cartodb positron")
-    folium.GeoJson(political_countries_url).add_to(m)
+    # m = folium.Map(location=(30, 10), zoom_start=3, tiles="cartodb positron")
+    # folium.GeoJson(political_countries_url).add_to(m)
 
-    m.save("footprint.html")
+    # m.save("footprint.html")
 
-    # m = leafmap.Map(locate_control=True)
-    # m.add_basemap("ROADMAP")
-    m.to_streamlit(height=700)
+    # # m = leafmap.Map(locate_control=True)
+    # # m.add_basemap("ROADMAP")
+    # m.to_streamlit(height=700)
