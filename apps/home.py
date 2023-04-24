0bde7547bc87ca3e227d2251082e2b992a7ad611
NicolasBelissent <nicolas.belissent@gmail.com>
2023-04-24 12:35:35 +0100

    image too big

--- apps/home.py
+++ apps/home.py
@@ -9,9 +9,7 @@ def app():
     Welcome to Geospatial à la fran​çaise, a geospatial web app that provides visualisations on arguably the pillars of french culture - cheese and wine. 
 
     I'm always looking for ways to improve my skills and learn new tools. Streamlit has quickly become a popular framework for building data-driven web applications, and I was excited to dive in and explore its capabilities. I've always enjoyed playing around with maps and geodata. This was a greate opportuinty to expand my geospatial data toolkit.
-    
-
 
     """
     )
-    st.image("data/blog.jpg")
+    st.image("data/blog.jpg", width=700)
