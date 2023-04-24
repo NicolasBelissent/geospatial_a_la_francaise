39346f7ee9dd3b8b1b7185f108bf2404260e6a93
NicolasBelissent <nicolas.belissent@gmail.com>
2023-04-24 12:32:39 +0100

    excla

--- apps/home.py
+++ apps/home.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import leafmap.foliumap as leafmap
-import folium
 
 
 def app():
@@ -13,7 +11,7 @@ def app():
     I'm always looking for ways to improve my skills and learn new tools. Streamlit has quickly become a popular framework for building data-driven web applications, and I was excited to dive in and explore its capabilities. I've always enjoyed playing around with maps and geodata. This was a greate opportuinty to expand my geospatial data toolkit.
     
 
-    [Geospatial à la fran​çaise](data/blog.jpg)
+    ![Geospatial à la fran​çaise](data/blog.jpg)
 
     """
     )
